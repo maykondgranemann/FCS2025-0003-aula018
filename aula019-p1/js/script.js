4e34c7c20331cd31d05991ab00d6a463f6928d00
Maykon D Granemann <maykondgranemann@gmail.com>
2025-03-12 20:56:50 -0300

    Aula 19 - P1 - Console, variavel e constante

---
+++ aula019-p1/js/script.js
@@ -0,0 +1,7 @@
+console.log('Mensagem do arquivo externo!');
+var nome = 'Maykon';
+let idade = 18;
+const PI = 3.1416;
+console.error('Não foi possivel carregar o usuario: ' + nome );
+
+console.clear();
