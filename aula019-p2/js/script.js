60abf99353881e37cc81834ca6ede1faa1a35e53
Maykon D Granemann <maykondgranemann@gmail.com>
2025-03-13 20:22:21 -0300

    Aula019-P2

---
+++ aula019-p2/js/script.js
@@ -0,0 +1,30 @@
+console.log('Aula 19 - P2 - operações!');
+
+var n1 = 10; //number
+var n2 = 15.8; //number
+
+console.log(`N1: ${n1}`);
+console.log(`N2: ${n2}`);
+
+var som = n1 + n2;
+var sub = n1 - n2;
+var mul = n1 * n2;
+var div = n1 / n2;
+
+console.log(`A soma é ${som}`);
+console.log(`A subtração é ${sub}`);
+console.log(`A multiplicação é ${mul}`);
+console.log(`A divisão é ${div}`);
+
+n1 = 'Olá';
+var nome = 'João';
+
+// Typagem
+console.log(typeof(n1));
+console.log(typeof(n2));
+console.log(typeof(som));
+console.log(typeof(sub));
+console.log(typeof(mul));
+console.log(typeof(div));
+console.log(typeof(nome));
+console.log(typeof(true));
