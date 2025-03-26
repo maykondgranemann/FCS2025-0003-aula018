faeaebc260b696b4f9351f388b86354d4fb00aa5
Maykon D Granemann <maykondgranemann@gmail.com>
2025-03-26 20:06:09 -0300

    Aula 22 - Node

---
+++ aula022/script-node.js
@@ -0,0 +1,9 @@
+console.log("Desenvolvendo com NODEJS!");
+
+var n1 = 10;
+var n2 = 20;
+var soma = n1 + n2;
+
+console.log(`A soma de ${n1} + ${n2} = ${soma}`);
+
+nome:string = "Joao";
---
+++ aula022/script.js
@@ -0,0 +1,16 @@
+console.log('Aula 20 - P2 - Listas');
+
+var n1 = 10;
+console.log(n1);
+
+var numeros = [10,86,99,40,52];
+
+
+console.log(numeros[4]);
+numeros[2] = 'João';
+console.log(numeros);
+console.log(numeros.length);
+numeros.push(67);
+console.log(numeros);
+numeros.pop();
+console.log(numeros);
